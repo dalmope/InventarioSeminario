820d8027a2efd532870d67dc037bc9b3733757bd
David Alberto Mora <80482968+dalmope@users.noreply.github.com>
2021-11-04 22:30:09 -0500

    Delete EmpleadoDAO.java

--- src/main/java/com/example/inventario/dao/EmpleadoDAO.java
+++
@@ -1,8 +0,0 @@
-package com.example.inventario.dao;
-
-import com.example.inventario.modelo.Empleado;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmpleadoDAO extends JpaRepository<Empleado, Integer> {
-
-}
